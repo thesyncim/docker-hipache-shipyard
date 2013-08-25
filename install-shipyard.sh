860e9bc88bf6cd4a0cc17cecfd61ef14a3b40798
thesyncim <thesyncim@gmail.com>
2013-08-25 16:37:40 -0700

    Update install-shipyard.sh

--- install-shipyard.sh
+++ install-shipyard.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
-sudo apt-get -qq update
-sudo apt-get install -y python-dev python-setuptools libxml2-dev libxslt-dev libmysqlclient-dev  git-core
+
+cd ~
+apt-get update -qq
+apt-get install -y linux-image-extra-`uname -r`
+
+wget https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
+wget -O - http://nodejs.org/dist/v0.8.23/node-v0.8.23-linux-x64.tar.gz | tar -C /usr/local/ --strip-components=1 -zxv
+tar xf go1.1.2.linux-amd64.tar.gz
+rm rf go1.1.2.linux-amd64.tar.gz
+
+echo "export GOROOT=\$HOME/go" >> ~/.profile
+echo "PATH=$PATH:\$GOROOT/bin" >> ~/.profile
+source ~/.profile
+
+mkdir ~/gocode
+echo "export GOPATH=\$HOME/gocode" >> ~/.profile
+echo "PATH=\$PATH:\$GOPATH/bin" >> ~/.profile
+source ~/.profile
+
+sudo apt-get  -y install lxc curl xz-utils mercurial git apt-get install -y python-dev python-setuptools libxml2-dev libxslt-dev libmysqlclient-dev  git-core redis-server
+
+
+mkdir -p $GOPATH/src/github.com/dotcloud
+
+
+cd $GOPATH/src/github.com/dotcloud
+git clone https://github.com/dotcloud/docker.git
+
+
+cd $GOPATH/src/github.com/dotcloud/docker
+go get -v github.com/dotcloud/docker/...
+
+ln -s $GOPATH/bin/docker /usr/local/bin/docker
+
+
+cd $GOPATH/src/github.com/dotcloud
+git clone https://github.com/dotcloud/hipache.git
+cd $GOPATH/src/github.com/dotcloud/hipache
+npm install hipache -g
+mkdir -p /var/log/supervisor
+cp supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+cp config/config_dev.json /usr/local/lib/node_modules/hipache/config/config_dev.json
+cp config/config_test.json /usr/local/lib/node_modules/hipache/config/config_test.json
+cp config/config.json /usr/local/lib/node_modules/hipache/config/config.json
+
+//install shipyard
 sudo easy_install pip
 sudo pip install virtualenv
 sudo pip install uwsgi
