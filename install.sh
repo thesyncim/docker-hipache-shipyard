1633c4637c3456d460742426d9726778a23c7b22
thesyncim <thesyncim@gmail.com>
2013-08-26 12:57:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,50 +18,43 @@ HIPACHECONF='
 }
 '
 cd ~
-apt-get update -qq
+apt-get update 
 apt-get install -y linux-image-extra-`uname -r`
+apt-get install -y lxc curl xz-utils mercurial git python-dev python-setuptools libxml2-dev libxslt-dev libmysqlclient-dev  git-core redis-server supervisor
 
+#install node
 wget -O - http://nodejs.org/dist/v0.8.23/node-v0.8.23-linux-x64.tar.gz | tar -C /usr/local/ --strip-components=1 -zxv
+
+#install go 
 mkdir $HOME/go && wget -O - https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz | tar -C $HOME/go/ --strip-components=1 -zxv
 echo "export GOROOT=\$HOME/go" >> ~/.profile
 echo "PATH=$PATH:\$GOROOT/bin" >> ~/.profile
-source ~/.profile
-
-mkdir ~/gocode
 echo "export GOPATH=\$HOME/gocode" >> ~/.profile
 echo "PATH=\$PATH:\$GOPATH/bin" >> ~/.profile
-echo 'alias docker="docker -H=tcp://127.0.0.1:4243"' >> ~/.profile
+mkdir ~/gocode
 source ~/.profile
 
-apt-get  install -y lxc curl xz-utils mercurial git python-dev python-setuptools libxml2-dev libxslt-dev libmysqlclient-dev  git-core redis-server supervisor
-
-wget -O /etc/supervisor/conf.d/supervisord-docker.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-docker.conf
-wget -O /etc/supervisor/conf.d/supervisord-hipache.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-hipache.conf
-wget -O /etc/supervisor/conf.d/supervisord-shipyard.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-shipyard.conf
 
 
+#install docker
 mkdir -p $GOPATH/src/github.com/dotcloud
-
-
 cd $GOPATH/src/github.com/dotcloud
 git clone https://github.com/dotcloud/docker.git
-
-
 cd $GOPATH/src/github.com/dotcloud/docker
 go get -v github.com/dotcloud/docker/...
-
 ln -s $GOPATH/bin/docker /usr/local/bin/docker
+echo 'alias docker="docker -H=tcp://127.0.0.1:4243"' >> ~/.profile
+wget -O /etc/supervisor/conf.d/supervisord-docker.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-docker.conf
 
-
+#install hipache
 cd $GOPATH/src/github.com/dotcloud
 git clone https://github.com/dotcloud/hipache.git
 cd $GOPATH/src/github.com/dotcloud/hipache
 npm install hipache -g
-
-
+wget -O /etc/supervisor/conf.d/supervisord-hipache.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-hipache.conf
 echo $HIPACHECONF > /usr/local/lib/node_modules/hipache/config/config.json
 
-
+#install shipyard
 sudo easy_install pip
 sudo pip install virtualenv
 sudo pip install uwsgi
@@ -74,3 +67,4 @@ sudo /opt/ve/shipyard/bin/pip install -r /opt/apps/shipyard/requirements.txt
 cd /opt/apps/shipyard && sudo /opt/ve/shipyard/bin/python manage.py syncdb --noinput
 cd /opt/apps/shipyard && sudo /opt/ve/shipyard/bin/python manage.py migrate
 cd /opt/apps/shipyard && sudo /opt/ve/shipyard/bin/python manage.py update_admin_user --username=admin --password=shipyard
+wget -O /etc/supervisor/conf.d/supervisord-shipyard.conf https://raw.github.com/thesyncim/docker-hipache-shipyard/master/supervisord-shipyard.conf
